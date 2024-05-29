8b3e237838964c7ff5c91e2d0b4f7569623db167
parkSJ <parksu987@gmail.com>
2024-05-29 12:28:04 +0900

    junit 자동화 체크를 위한 코드 작성

--- src/main/java/org/example/Admin.java
+++ src/main/java/org/example/Admin.java
@@ -1,4 +1,8 @@
 package org.example;
 
 public class Admin {
+
+    public static int testPlus(int num1, int num2) {
+        return num1+num2;
+    }
 }
--- src/test/java/JUnitTest.java
+++ src/test/java/JUnitTest.java
@@ -1,6 +1,19 @@
+import org.example.Admin;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class JUnitTest {
+    @Test
+    void test1(){
+        int result = Admin.testPlus(1, 2);
+        assertEquals(3, result);
+    }
 
-
+    @Test
+    void errorTest1() {
+        int result = Admin.testPlus(1, 2);
+        assertEquals(4, result);
+    }
 }
