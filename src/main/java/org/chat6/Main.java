07fdb82b6e42b2ab3d2a59de2989ac4ad61c1d01
sontouf <sontouf@gmail.com>
2024-06-06 20:25:22 +0900

    check code

--- src/main/java/org/chat6/Main.java
+++ src/main/java/org/chat6/Main.java
@@ -3,6 +3,19 @@ package org.chat6;
 
 public class Main {
     public static void main(String[] args) {
+        AuthenticationCode authenticationCode = new AuthenticationCode();
+        System.out.println(authenticationCode.generateRandomString());
+        System.out.println(authenticationCode.generateRandomString());
+        System.out.println(authenticationCode.generateRandomString());
+
+        System.out.println(authenticationCode.checkvalidCode(authenticationCode.generateRandomString()));
+        System.out.println(authenticationCode.checkvalidCode("012"));
+        System.out.println(authenticationCode.checkvalidCode("012abdcABC"));
+        System.out.println(authenticationCode.checkvalidCode("012abcdadd"));
+        System.out.println(authenticationCode.checkvalidCode("012abcdadPd"));
+
+
+
         VMController vmController = new VMController("team8", 30, 75);
         vmController.init();
 
