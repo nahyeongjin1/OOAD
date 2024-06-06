63195bb2f8aa351232fa81d74e18ad90dd075305
sontouf <sontouf@gmail.com>
2024-06-06 20:22:18 +0900

    code make

--- src/main/java/org/chat6/AuthenticationCode.java
+++ src/main/java/org/chat6/AuthenticationCode.java
@@ -1,9 +1,7 @@
 package org.chat6;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class AuthenticationCode {
     List<Map<String, Object>> list = new ArrayList<>();
@@ -45,4 +43,63 @@ public class AuthenticationCode {
             }
         }
     }
+
+    public boolean checkvalidCode(String code) {
+        boolean digit = false;
+        boolean lowerAlpha = false;
+        boolean capital = false;
+        if (code.length() != 10)
+            return false;
+        for (int i = 0; i < 10; i++) {
+            if (code.charAt(i) <= 'z' && code.charAt(i) >= 'a') {
+                lowerAlpha = true;
+                continue;
+            }
+            else if (code.charAt(i) <= 'Z' && code.charAt(i) >= 'A') {
+                capital = true;
+                continue;
+            }
+            else if (code.charAt(i) <= '9' && code.charAt(i) >= '0') {
+                digit = true;
+                continue;
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public String generateRandomString() {
+        String uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+        String digits = "0123456789";
+
+        StringBuilder result = new StringBuilder();
+        Random random = new Random();
+
+        // Add one uppercase letter
+        result.append(uppercaseLetters.charAt(random.nextInt(uppercaseLetters.length())));
+
+        // Add one lowercase letter
+        result.append(lowercaseLetters.charAt(random.nextInt(lowercaseLetters.length())));
+
+        // Add one digit
+        result.append(digits.charAt(random.nextInt(digits.length())));
+
+        // Add seven random characters (letters or digits)
+        for (int i = 0; i < 7; i++) {
+            int randomIndex = random.nextInt(uppercaseLetters.length() + lowercaseLetters.length() + digits.length());
+            if (randomIndex < uppercaseLetters.length()) {
+                result.append(uppercaseLetters.charAt(randomIndex));
+            } else if (randomIndex < uppercaseLetters.length() + lowercaseLetters.length()) {
+                result.append(lowercaseLetters.charAt(randomIndex - uppercaseLetters.length()));
+            } else {
+                result.append(digits.charAt(randomIndex - uppercaseLetters.length() - lowercaseLetters.length()));
+            }
+        }
+
+        return result.toString();
+    }
 }
+
